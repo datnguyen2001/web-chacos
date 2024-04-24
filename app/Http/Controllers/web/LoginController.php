b5c868c6fc6f9e1e612fc1e0da6559cc397d5486
tungnh3011.work <tungnh3011.work@gmail.com>
2024-04-25 00:15:51 +0700

    signIn signUp

--- app/Http/Controllers/web/LoginController.php
+++ app/Http/Controllers/web/LoginController.php
@@ -2,8 +2,15 @@
 
 namespace App\Http\Controllers\web;
 
+use App\Enums\UserStatus;
 use App\Http\Controllers\Controller;
+use App\Models\User;
+use App\Rules\NoSpaceRule;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Str;
 
 class LoginController extends Controller
 {
@@ -11,8 +18,95 @@ class LoginController extends Controller
     {
         return view('web.login');
     }
+
     public function registration()
     {
         return view('web.registration');
+    }
+
+    public function signUp(Request $request)
+    {
+        try {
+            $validated = Validator::make($request->all(), [
+                'first_name' => 'required|string|max:20',
+                'last_name' => 'required|string|max:20',
+                'email' => 'required|email|unique:users,email|max:30',
+                'email_confirmed' => 'required|email|same:email',
+                'password' => ['required', 'string', 'min:8', 'regex:/[A-Z]/', 'regex:/[a-z]/', 'regex:/[0-9]/', 'regex:/[\W]/', new NoSpaceRule],
+                'password_confirmed' => ['required', 'same:password']
+            ]);
+
+            if ($validated->fails()) {
+                toastr()->error($validated->errors()->first());
+                return back()->withInput();
+            }
+
+            $validatedData = $validated->validated();
+
+            User::create([
+                'first_name' => $validatedData['first_name'],
+                'last_name' => $validatedData['last_name'],
+                'email' => $validatedData['email'],
+                'password' => Hash::make($validatedData['password']),
+                'status' => UserStatus::ACTIVE,
+            ]);
+
+            toastr()->success('Đăng ký thành công với email ' . $validatedData['email']);
+            return redirect()->route('login');
+        } catch (\Exception $e) {
+            toastr()->error($e->getMessage());
+            return back()->withInput();
+        }
+    }
+
+    public function signIn(Request $request)
+    {
+        try {
+            $validated = Validator::make($request->all(), [
+                'email' => 'required|email|exists:users,email|max:30',
+                'password' => ['required', 'min:8', new NoSpaceRule],
+            ]);
+
+            if ($validated->fails()) {
+                toastr()->error($validated->errors()->first());
+                return back()->withInput();
+            }
+
+            $validatedData = $validated->validated();
+
+            $email = $validatedData['email'];
+            $password = $validatedData['password'];
+
+            $user = User::where('email', $email)->first();
+
+            if ($user && Hash::check($password, $user->password)) {
+                if ($user->status == 1) {
+                    Auth::login($user, $request->has('remember'));
+
+                    toastr()->success('Welcome ' . $user->first_name . ' ' . $user->last_name);
+                    return redirect()->intended(route('home'));
+                } else {
+                    toastr()->error("Your account is not activated yet");
+                    return back()->withInput();
+                }
+            }
+
+            toastr()->error("Wrong email or password");
+            return back()->withInput();
+        } catch (\Exception $e) {
+            toastr()->error($e->getMessage());
+            return back()->withInput();
+        }
+    }
+
+    public function logout()
+    {
+        if (Auth::check()) {
+            Auth::logout();
+            toastr()->success('Goodbye & Hope to you again');
+            return redirect()->route('home');
+        }
+        toastr()->success('Something when wrong');
+        return back();
     }
 }
--- resources/views/web/registration.blade.php
+++ resources/views/web/registration.blade.php
@@ -1,99 +1,116 @@
 @extends('web.index')
-@section('title','Đăng Ký')
+@section('title', 'Đăng Ký')
 
 @section('style_page')
-    <link rel="stylesheet" href="{{asset('assets/css/login.css')}}">
+    <link rel="stylesheet" href="{{ asset('assets/css/login.css') }}">
 @stop
-{{--content of page--}}
+{{-- content of page --}}
 
 @section('content')
+    @php
+        $url = parse_url(env('APP_URL'));
+        $domain = $url['host'];
+    @endphp
     <div class="box-registration">
         <div class="line-header-menu-page">
-            <a href="{{route('home')}}" class="title-header-menu-page">Trang chủ</a>
+            <a href="{{ route('home') }}" class="title-header-menu-page">Trang chủ</a>
             <span style="margin: 0 5px;">/</span>
-            <a href="{{route('registration')}}" class="title-header-menu-page">Đăng ký tài khoản</a>
+            <a href="{{ route('registration') }}" class="title-header-menu-page">Đăng ký tài khoản</a>
         </div>
         <div class="box-content-reg">
             <div class="box-left-registration">
                 <p class="title-menu-child-reg">Tài khoản</p>
-                <a href="{{route('login')}}" class="link-page-reg">Đăng nhập</a>
+                <a href="{{ route('login') }}" class="link-page-reg">Đăng nhập</a>
             </div>
             <div class="box-right-registration">
                 <p class="title-reg">Đăng ký tài khoản</p>
-                <div class="box-info-reg">
-                    <div class="line-header-info-reg">
-                        <p class="title-info-big-reg">Thông tin cá nhân</p>
-                        <p class="title-info-small-reg">* Phần bắt buộc</p>
-                    </div>
-                    <div class="content-info-reg">
-                        <div class="col-left-info">
-                            <label class="title-lable-info">* First Name</label>
-                            <input type="text" class="input-value-reg">
-                            <label class="title-lable-info">* Last Name</label>
-                            <input type="text" class="input-value-reg">
+                <form action="{{ route('sign-up') }}" method="POST" autocomplete="off">
+                    @csrf
+                    <div class="box-info-reg">
+                        <div class="line-header-info-reg">
+                            <p class="title-info-big-reg">Thông tin cá nhân</p>
+                            <p class="title-info-small-reg">* Phần bắt buộc</p>
                         </div>
-                        <div class="col-right-info">
-                            <p class="title-note">Đây là tên sẽ được liên kết với tài khoản Chaco.com của bạn.</p>
+                        <div class="content-info-reg">
+                            <div class="col-left-info">
+                                <label class="title-lable-info" for="first_name">* First Name</label>
+                                <input id="first_name" type="text" class="input-value-reg" name="first_name"
+                                    value="{{ old('first_name') }}" autofocus>
+                                <label class="title-lable-info" for="last_name">* Last Name</label>
+                                <input id="last_name" type="text" class="input-value-reg" name="last_name"
+                                    value="{{ old('last_name') }}">
+                            </div>
+                            <div class="col-right-info">
+                                <p class="title-note">Đây là tên sẽ được liên kết với tài khoản {{ $domain }} của bạn.
+                                </p>
+                            </div>
                         </div>
-                    </div>
-                    <div class="line-header-info-reg">
-                        <p class="title-info-big-reg">Địa chỉ email</p>
-                        <p class="title-info-small-reg">* Phần bắt buộc</p>
-                    </div>
-                    <div class="content-info-reg">
-                        <div class="col-left-info">
-                            <label class="title-lable-info">* Email</label>
-                            <input type="text" class="input-value-reg">
-                            <label class="title-lable-info">* Xác nhận email</label>
-                            <input type="text" class="input-value-reg">
+                        <div class="line-header-info-reg">
+                            <p class="title-info-big-reg">Địa chỉ email</p>
+                            <p class="title-info-small-reg">* Phần bắt buộc</p>
                         </div>
-                        <div class="col-right-info">
-                            <p class="title-note">Bạn sẽ sử dụng địa chỉ email của mình để đăng nhập vào Chacos.com. Chúng tôi
-                                yêu cầu bạn nhập hai lần để đảm bảo độ chính xác.</p>
+                        <div class="content-info-reg">
+                            <div class="col-left-info">
+                                <label class="title-lable-info" for="email">* Email</label>
+                                <input id="email" type="text" class="input-value-reg" name="email"
+                                    value="{{ old('email') }}">
+                                <label class="title-lable-info" for="email_confirmed">* Xác nhận email</label>
+                                <input id="email_confirmed" type="text" class="input-value-reg" name="email_confirmed"
+                                    value="{{ old('email_confirmed') }}">
+                            </div>
+                            <div class="col-right-info">
+                                <p class="title-note">Bạn sẽ sử dụng địa chỉ email của mình để đăng nhập vào
+                                    {{ $domain }}. Chúng tôi
+                                    yêu cầu bạn nhập hai lần để đảm bảo độ chính xác.</p>
+                            </div>
                         </div>
-                    </div>
-                    <div class="line-header-info-reg">
-                        <p class="title-info-big-reg">Mật khẩu</p>
-                        <p class="title-info-small-reg">* Phần bắt buộc</p>
-                    </div>
-                    <div class="content-info-reg">
-                        <div class="col-left-info">
-                            <label class="title-lable-info">* Mật khẩu 8 - 20 ký tự</label>
-                            <input type="text" class="input-value-reg">
-                            <label class="title-lable-info">* Xác nhận mật khẩu</label>
-                            <input type="text" class="input-value-reg">
+                        <div class="line-header-info-reg">
+                            <p class="title-info-big-reg">Mật khẩu</p>
+                            <p class="title-info-small-reg">* Phần bắt buộc</p>
                         </div>
-                        <div class="col-right-info">
-                            <p class="title-note">Tạo mật khẩu cho tài khoản Chacos.com của bạn. Mật khẩu của bạn phải dài từ
-                                8-20 ký tự. Nó phải chứa 1 trong số những điều sau đây:</p>
-                            <ul style="list-style:inherit;">
-                                <li>
-                           <span style="vertical-align: inherit;">
-                              <span style="vertical-align: inherit;">Chữ hoa</span>
-                           </span>
-                                </li>
-                                <li>
-                           <span style="vertical-align: inherit;">
-                              <span style="vertical-align: inherit;">Chữ thường</span>
-                           </span>
-                                </li>
-                                <li>
-                           <span style="vertical-align: inherit;">
-                              <span style="vertical-align: inherit;">số</span>
-                           </span>
-                                </li>
-                                <li>
-                           <span style="vertical-align: inherit;">
-                              <span style="vertical-align: inherit;">Tính cách đặc biệt</span>
-                           </span>
-                                </li>
-                            </ul>
-                            <p class="title-note">
-                                Không gian không được phép. Chúng tôi yêu cầu bạn nhập hai lần để đảm bảo độ chính xác.</p>
+                        <div class="content-info-reg">
+                            <div class="col-left-info">
+                                <label class="title-lable-info" for="password">* Mật khẩu 8 - 20 ký tự</label>
+                                <input id="password" name="password" type="password" class="input-value-reg">
+                                <label class="title-lable-info" for="password_confirmed">* Xác nhận mật khẩu</label>
+                                <input id="password_confirmed" name="password_confirmed" type="password"
+                                    class="input-value-reg">
+                            </div>
+                            <div class="col-right-info">
+                                <p class="title-note">Tạo mật khẩu cho tài khoản {{ $domain }} của bạn. Mật khẩu của
+                                    bạn
+                                    phải dài từ
+                                    8-20 ký tự. Nó phải chứa 1 trong số những điều sau đây:</p>
+                                <ul style="list-style:inherit;">
+                                    <li>
+                                        <span style="vertical-align: inherit;">
+                                            <span style="vertical-align: inherit;">Chữ hoa</span>
+                                        </span>
+                                    </li>
+                                    <li>
+                                        <span style="vertical-align: inherit;">
+                                            <span style="vertical-align: inherit;">Chữ thường</span>
+                                        </span>
+                                    </li>
+                                    <li>
+                                        <span style="vertical-align: inherit;">
+                                            <span style="vertical-align: inherit;">Số</span>
+                                        </span>
+                                    </li>
+                                    <li>
+                                        <span style="vertical-align: inherit;">
+                                            <span style="vertical-align: inherit;">Kí tự đặc biệt</span>
+                                        </span>
+                                    </li>
+                                </ul>
+                                <p class="title-note">
+                                    Không gian không được phép. Chúng tôi yêu cầu bạn nhập hai lần để đảm bảo độ chính xác.
+                                </p>
+                            </div>
                         </div>
+                        <button type="submit" class="btn-registrantion">Đăng ký</button>
                     </div>
-                    <button class="btn-registrantion">Đăng ký</button>
-                </div>
+                </form>
             </div>
         </div>
     </div>
